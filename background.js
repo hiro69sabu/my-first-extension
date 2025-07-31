1df171d7d3b532b1f71111e28d273eb55c027959
hiro69sabu <ak.hiro69sabu@gmail.com>
2025-07-31 23:03:30 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,2 +1,17 @@
-// This is the background script of the extension.
-console.log("Background script loaded.");
+// このファイルは拡張機能のバックグラウンドで動作するスクリプトです。
+
+// 拡張機能がインストールされた、またはアップデートされた時に一度だけ実行されるイベント
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('My First Extension - Background script installed.');
+});
+
+// すべての通信リクエストを監視するイベントリスナー
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    // リクエストの詳細の中から、URLだけをコンソールに出力する
+    console.log("Request URL:", details.url);
+  },
+  {
+    urls: ["<all_urls>"] // すべてのURLを対象とする
+  }
+);
